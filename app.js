58d0222f18dd6cf54715a331073daffcf1d5737a
findfrankie <106564473+findfrankie@users.noreply.github.com>
2022-11-15 15:26:02 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+// const cardButton   = document.querySelector(".card Button");
+// const payment = document.querySelector(".payment");
+// const close = document.querySelector(".myClose");
+
+//  cardButton.addEventListener ("click", ()=>{
+//     payment.style.display ="flex"
+// })
+
+// close.addEventListener ("click",()=>{
+//     payment.style.display ="none"
+// })
+
+
+
+function myTutton() {
+    document.getElementById("myPayment").style.display = "flex";
+    
+  }
+
+  function cardButton() {
+    document.getElementById("myPayment").style.display = "flex";
+    
+  }
+
+  function myClose() {
+    document.getElementById("myPayment").style.display = "none";
+    
+  }
+
+ 
